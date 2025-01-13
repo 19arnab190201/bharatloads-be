f244db66ce59100a152b0c55c15e56703e56e98d
Ishan Audichya <audichyaishan@gmail.com>
2025-01-13 23:54:26 +0530

    add location api

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require('../middlewares/auth');
-
-const { home, getDashboard} = require("../controllers/homeControllers");
+const { home, getDashboard, getLocation } = require("../controllers/homeControllers");
 
 router.route("/home").get(home);
 router.route("/dashboard").get(protect, getDashboard);
+router.route("/locationsearch").get(getLocation);
 
 
 module.exports = router;
