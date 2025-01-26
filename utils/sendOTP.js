0c41aa9d242a1d265db2f815ccbef38fc6ac4ed4
Ishan Audichya <ishan@trademarkia.com>
2025-01-27 01:30:35 +0530

    major bug fixes2

--- utils/sendOTP.js
+++ utils/sendOTP.js
@@ -1,6 +1,10 @@
 const axios = require("axios");
 
 exports.sendOTP = async (phone, otp) => {
+    if(process.env.ENVIRONMENT === "development"){
+        console.log('OTP', otp, phone);
+        return true
+    }
     try {
         const response = await axios.get(
             `https://www.fast2sms.com/dev/bulkV2?authorization=${process.env.FAST2SMS_API_KEY}&route=dlt&sender_id=${process.env.FAST2SMS_SENDER_ID}&message=${process.env.FAST2SMS_MESSAGE}&variables_values=${otp}%7C&flash=0&numbers=${phone}`
